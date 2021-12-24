b46a6bd8c29089b53e3e65991551ea35662adc3f
Artem <malpolon76@gmail.com>
2021-12-24 19:41:26 +0200

    alert component work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AlertComponent } from './shared/alert/alert.component';
+import { GameService } from './shared/game.service';
+import { PlaceholderDirective } from './shared/placeholder.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy, OnInit {
   title = 'comparus-test';
+
+  @ViewChild(PlaceholderDirective, {static: false}) alertHost: PlaceholderDirective
+  private CloseSub!: Subscription
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private gameSefvice: GameService,) {}
+  
+  ngOnInit(): void {
+    this.gameSefvice.gameEvent.subscribe((eventStatus: string) => {
+      if( eventStatus === 'finished') {
+        console.log("From AppComponent, who is the Winner?: " + this.gameSefvice.winner)
+        this.showGameResults(this.gameSefvice.winner)
+      }
+    })
+  }
+
+  private showGameResults (winner: string) {
+
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(AlertComponent)
+
+    const hostViewContainerRef = this.alertHost.viewContainerRef
+    hostViewContainerRef.clear() 
+
+    const componentRef = hostViewContainerRef.createComponent(componentFactory)
+    componentRef.instance.winner = winner
+    
+  }
+
+  ngOnDestroy(): void {
+      if( this.CloseSub) {
+        this.CloseSub.unsubscribe()
+      }
+  }
 }
